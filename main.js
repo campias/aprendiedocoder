ec2822ed572f693bc0012aac419ba3d503a5f7db
campias <79773744+campias@users.noreply.github.com>
2021-02-27 22:57:44 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+let numero1 = prompt("Ingrese un numero");
+let numero2 = prompt("Ingrese otro numero");
+let prueba = 1000000;
+let suma = numero1 + numero2;
+if (suma > prueba) {
+  console.log("Usted es millonario")
+} else {
+  console.log("A trabajar!!")
+}
